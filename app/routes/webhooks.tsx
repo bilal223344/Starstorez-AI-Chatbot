2264437537844a72b484afbca41a25bb13b6f887
abdul-ahad-dev <abdulahad200325@gmail.com>
2026-01-31 23:12:24 +0500

    feat: Add Shopify webhook endpoint and core script asset, updating app configuration.

---
+++ app/routes/webhooks.tsx
@@ -0,0 +1,12 @@
+import { ActionFunctionArgs } from "react-router";
+import { authenticate } from "../shopify.server";
+
+
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+
+    await authenticate.webhook(request);
+
+    return new Response();
+
+};
